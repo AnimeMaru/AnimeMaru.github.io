60037dc8f7a68d6af766833b0f4f0c1718ebdaeb
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-17 21:33:08 -0500

    Working

--- haremViz/script.js
+++ haremViz/script.js
@@ -5,4 +5,10 @@ $( "#saber" ).draggable({
 var appendTo = $( "#saber" ).draggable( "option", "appendTo" );
 
 $( "#saber" ).draggable( "option", "appendTo", "body" );
+
+/*
+$("#saber").draggable({
+	containment: $("#square")
+})
+*/
  
